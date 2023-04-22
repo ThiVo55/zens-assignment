84d01a1d4897fd58bc54e2d1645204286fb65f1c
THIOB\user <dinhthi1996@gmail.com>
2023-04-22 18:03:26 +0700

    algorithm.js

---
+++ algorithm.js
@@ -0,0 +1,23 @@
+const str = "1 2 3 4 5";
+
+const words = str.split(" ");
+
+function sumArray(arr) {
+  let min,max,sum = 0;
+  let even = [];
+  let odd = [];
+  let result;
+
+  for (const element of words) {
+    let a = Number(element);
+    sum += a;
+    if (a % 2 == 1) odd.push(element);
+    else even.push(element);
+  }
+  max = sum - [...words].shift();
+  min = sum - [...words].pop();
+
+  return (result = `min:${min}  max:${max} odd:${odd} even:${even} sum:${sum}`);
+}
+
+console.log(sumArray(words)); // 25
